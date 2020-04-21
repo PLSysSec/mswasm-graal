b9e6655a00291c92ed7bb500c9d5a2f6e78770d3
Alexandra Michael <aemichae@ucsd.edu>
2020-04-21 13:07:54 -0700

    Added Handle back

---
+++ wasm/src/org.graalvm.wasm/src/org/graalvm/wasm/mswasm/Handle.java
@@ -0,0 +1,86 @@
+package org.graalvm.wasm.mswasm;
+
+public class Handle {
+    private int base;
+    private int offset;
+    private int bound;
+    private boolean isCorrupted;
+
+    public Handle(int base, int offset, int bound, boolean isCorrupted) {
+        this.base = base;
+        this.offset = offset;
+        this.bound = bound;
+        this.isCorrupted = isCorrupted;
+    }
+
+    public Handle(Handle other) {
+        this.base = other.getBase();
+        this.offset = other.getOffset();
+        this.bound = other.getBound();
+        this.isCorrupted = other.isCorrupted();
+    }
+
+    @Override
+    public String toString() {
+        return "Handle: (" + base + ", " + offset + ", " + bound + ", " + isCorrupted + ")"; 
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if ( ! (obj instanceof Handle)) {
+            return false;
+        }
+
+        Handle handle = (Handle) obj;
+        return handle.getOffset() == offset && handle.getBase() == base && 
+               handle.getBound() == bound;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.toString().hashCode();
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    public int getBase() {
+        return base;
+    }
+
+    public int getBound() {
+        return bound;
+    }
+
+    public boolean isCorrupted() {
+        return isCorrupted;
+    }
+
+    public void setCorrupted() {
+        isCorrupted = true;
+    }
+
+    public boolean isValid() {
+        return ! isCorrupted && base + offset <= bound && base + offset >= base;
+    }
+
+    /* MSWASM-TODO: remove?
+    // Converts to long containing base & offset
+    public long convertToLongBaseOffset() {
+        return ((long)base << 32) | ((long)offset);
+    }
+
+    // Converts to long containing bound & isCorrupted
+    public long convertToLongBoundIsC() {
+        return ((long)bound << 32) | ((long)isCorrupted);
+    }
+
+    public static Handle retrieveFromLong(long baseOffset, long boundIsC) {
+        int base = (int) (baseOffset >> 32);
+        int offset = (int) baseOffset;
+        int bound = (int) (boundIsC >> 32);
+        boolean isCorrupted = (boolean) boundIsC;
+        return new Handle(base, offset, bound, isCorrupted);
+    }*/
+}
