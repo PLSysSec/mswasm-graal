eae267950b8518884e4c34bcf378005cc6c9c0be
Aidan Denlinger <adenling@ucsd.edu>
2020-05-19 21:40:33 -0700

    restructured folders and added WasmComparisonSuite

---
+++ wasm/src/org.graalvm.wasm.test/src/org/graalvm/wasm/test/suites/mswasm/WasmComparisonSuite.java
@@ -0,0 +1,56 @@
+package org.graalvm.wasm.test.suites.mswasm;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.graalvm.wasm.utils.cases.WasmCase;
+import org.graalvm.wasm.utils.cases.WasmBinaryCase;
+import org.graalvm.wasm.utils.cases.WasmCaseData;
+import org.junit.Test;
+
+import org.graalvm.wasm.test.WasmSuiteBase;
+import java.io.InputStream;
+import java.io.FileInputStream;
+
+import java.util.Properties;
+
+public class WasmComparisonSuite extends WasmSuiteBase {
+    private String folderPath = "./src/org.graalvm.wasm.test/src/org/graalvm/wasm/test/suites/mswasm/wasmTests/";
+
+    private WasmBinaryCase[] testCases = {
+                    WasmCase.create("ADD_1", WasmCase.expected(11),
+                                    parseWasmFile(folderPath + "wasm_add_1.wasm"),
+                                    null, new Properties()), 
+    };
+
+    private byte[] parseWasmFile(String fileName) {
+        try {
+            File file = new File(fileName);
+            InputStream input = new FileInputStream(file);
+            byte[] bytes = new byte[(int) file.length()];
+            input.read(bytes);
+            input.close();
+            return bytes;
+        } catch (RuntimeException | IOException e) {
+            e.printStackTrace();
+            return new byte[0];  
+        }
+    }
+                    
+    @Override
+    protected Collection<? extends WasmCase> collectStringTestCases() {
+        return Arrays.asList(testCases);
+    }
+
+    @Override
+    @Test
+    public void test() throws IOException {
+        // This is here just to make mx aware of the test suite class.
+        super.test();
+    }
+}
