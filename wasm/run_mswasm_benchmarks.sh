a62a8ccdfe215c654818f09982ccb7445aa6a54c
Eric Siu <esiu@ucsd.edu>
2021-07-07 22:06:47 -0700

    added 1M and modified run benchmarks script"

--- wasm/run_mswasm_benchmarks.sh
+++ wasm/run_mswasm_benchmarks.sh
@@ -1,4 +1,10 @@
-mx --dy /truffle,/compiler benchmark wasm:WASM_BENCHMARKCASES -- --jvm=server -Dwasmbench.benchmarkName=mswasm_loop -- MSWasmBenchmarkSuite > ~/logs/mswasm_loop_benchmark.txt
-mx --dy /truffle,/compiler benchmark wasm:WASM_BENCHMARKCASES -- --jvm=server -Dwasmbench.benchmarkName=mswasm_loop2 -- MSWasmBenchmarkSuite > ~/logs/mswasm_loop2_benchmark.txt
-mx --dy /truffle,/compiler benchmark wasm:WASM_BENCHMARKCASES -- --jvm=server -Dwasmbench.benchmarkName=mswasm_salsa20 -- MSWasmBenchmarkSuite > ~/logs/mswasm_salsa20_benchmark.txt
-mx --dy /truffle,/compiler benchmark wasm:WASM_BENCHMARKCASES -- --jvm=server -Dwasmbench.benchmarkName=mswasm_store-load_1 -- MSWasmBenchmarkSuite > ~/logs/mswasm_store_load_1_benchmark.txt
+
+x=`date +%m_%d_%Y_%H_%M_%S`
+folder="/home/eric/benchmark_logs/mswasm_$x"
+
+mkdir "$folder"
+
+mx --dy /truffle,/compiler benchmark wasm:WASM_BENCHMARKCASES -- --jvm=server -Dwasmbench.benchmarkName=mswasm_loop -- MSWasmBenchmarkSuite > "$folder/mswasm_loop_benchmark.txt"
+mx --dy /truffle,/compiler benchmark wasm:WASM_BENCHMARKCASES -- --jvm=server -Dwasmbench.benchmarkName=mswasm_loop_1M -- MSWasmBenchmarkSuite > "$folder/mswasm_loop_1M_benchmark.txt"
+mx --dy /truffle,/compiler benchmark wasm:WASM_BENCHMARKCASES -- --jvm=server -Dwasmbench.benchmarkName=mswasm_salsa20 -- MSWasmBenchmarkSuite > "$folder/mswasm_salsa20_benchmark.txt"
+mx --dy /truffle,/compiler benchmark wasm:WASM_BENCHMARKCASES -- --jvm=server -Dwasmbench.benchmarkName=mswasm_store-load_1 -- MSWasmBenchmarkSuite > "$folder/mswasm_store_load_1_benchmark.txt"
