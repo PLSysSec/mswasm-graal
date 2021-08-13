0336018ae829a21079fc7a1c4d7bee141823faea
Aidan Denlinger <adenling@ucsd.edu>
2021-08-12 20:17:57 -0700

    importing module in a test case is working

--- wasm/src/org.graalvm.wasm.test/src/org/graalvm/wasm/test/suites/mswasm/CompMSWasmSuite.java
+++ wasm/src/org.graalvm.wasm.test/src/org/graalvm/wasm/test/suites/mswasm/CompMSWasmSuite.java
@@ -7,6 +7,8 @@ import java.util.Scanner;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
+import java.util.ArrayList;
 
 import org.graalvm.wasm.utils.cases.WasmCase;
 import org.graalvm.wasm.utils.cases.WasmBinaryCase;
@@ -21,51 +23,58 @@ import java.util.Properties;
 import org.graalvm.wasm.utils.SystemProperties;
 
 public class CompMSWasmSuite extends CompWasmSuiteBase {
-        private String folderPath = "./src/org.graalvm.wasm.test/src/org/graalvm/wasm/test/suites/mswasm/craig/";
+    private String folderPath = "./src/org.graalvm.wasm.test/src/org/graalvm/wasm/test/suites/mswasm/craig/";
 
-        private static Properties opts = SystemProperties.createFromOptions(
-                        "zero-memory = false\n" + "interpreter-iterations = 0\n" + "sync-noinline-iterations = 10\n"
-                                        + "sync-inline-iterations = 10\n" + "async-iterations = 10\n");
+    private static Properties opts = SystemProperties.createFromOptions(
+                    "zero-memory = false\n" + "interpreter-iterations = 0\n" + "sync-noinline-iterations = 10\n"
+                                    + "sync-inline-iterations = 10\n" + "async-iterations = 10\n");
 
-        private WasmBinaryCase[] testCases = {
-                        WasmCase.create("HACL_CHACHA20_CORE", WasmCase.expected(0),
-                                        parseWasmFile(folderPath + "Hacl_Chacha20_core.wasm"), null, opts),
-                        WasmCase.create("CRAIG_1", WasmCase.expected(0),
-                                         parseWasmFile(folderPath + "test1.wasm"), null, opts),
-                        WasmCase.create("CRAIG_2", WasmCase.expected(0),
-                                         parseWasmFile(folderPath + "test2.wasm"), null, opts),
-                        WasmCase.create("CRAIG_4", WasmCase.expected(0),
-                                         parseWasmFile(folderPath + "test4.wasm"), null, opts)
-        };
+    private WasmCase[] testCases = {
+        WasmCase.create("HACL_CHACHA20_CORE", WasmCase.expected(0),
+                        parseWasmFile(folderPath + "Hacl_Chacha20_core.wasm"), null, opts),
+        WasmCase.create("CRAIG_1", WasmCase.expected(0),
+                         parseWasmFile(folderPath + "test1.wasm"), null, opts),
+        WasmCase.create("CRAIG_2", WasmCase.expected(0),
+                         parseWasmFile(folderPath + "test2.wasm"), null, opts),
+        WasmCase.create("CRAIG_4", WasmCase.expected(0),
+                         parseWasmFile(folderPath + "test4.wasm"), null, opts)
+    };
 
-        private byte[] parseWasmFile(String fileName) {
-                try {
-                        File file = new File(fileName);
-                        InputStream input = new FileInputStream(file);
-                        byte[] bytes = new byte[(int) file.length()];
-                        input.read(bytes);
-                        input.close();
-                        return bytes;
-                } catch (RuntimeException | IOException e) {
-                        e.printStackTrace();
-                        return new byte[0];
-                }
+    private byte[] parseWasmFile(String fileName) {
+        try {
+            File file = new File(fileName);
+            InputStream input = new FileInputStream(file);
+            byte[] bytes = new byte[(int) file.length()];
+            input.read(bytes);
+            input.close();
+            return bytes;
+        } catch (RuntimeException | IOException e) {
+            e.printStackTrace();
+            return new byte[0];
         }
+    }
 
-        @Override
-        protected Collection<? extends WasmCase> collectStringTestCases() {
-                return Arrays.asList(testCases);
-        }
+    @Override
+    protected Collection<? extends WasmCase> collectStringTestCases() {
+        ArrayList<WasmCase> cases = new ArrayList<WasmCase>(Arrays.asList(testCases));
+        try {
+            cases.add(
+                WasmCase.collectFileCase("test", "mswasm", 
+                                         "wasi-libc/wasi-libc;wasi-libc-test")
+            );
+        } catch (IOException e) {}
+        return cases;
+    }
 
-        @Override
-        protected String includedExternalModules() {
-            return super.includedExternalModules() + ",memory,wasi,wasi_snapshot_preview1";
-        }
+    @Override
+    protected String includedExternalModules() {
+        return super.includedExternalModules() + ",memory,wasi,wasi_snapshot_preview1";
+    }
 
-        @Override
-        @Test
-        public void test() throws IOException {
-                // This is here just to make mx aware of the test suite class.
-                super.test();
-        }
+    @Override
+    @Test
+    public void test() throws IOException {
+            // This is here just to make mx aware of the test suite class.
+            super.test();
+    }
 }
--- wasm/src/org.graalvm.wasm.test/src/test/mswasm/wasi-libc/src/wasi-libc-test.wat
+++ wasm/src/org.graalvm.wasm.test/src/test/mswasm/wasi-libc/src/wasi-libc-test.wat
@@ -1,15 +1,18 @@
 (module
   (type (;0;) (func (param i32) (result handle)))
   (type (;1;) (func (param handle)))
-  (type (;2;) (func (result i32))
+  (type (;2;) (func (result i32)))
   (import "wasi-libc" "malloc" (func $__malloc (type 0)))
   (import "wasi-libc" "free" (func $__free (type 1)))
-  (func (export "_start") (type 0) (local (;0;) handle)
+  (func (export "_start") (type 2) (local (;0;) handle)
+    i32.const 4
     call $__malloc
     local.set 0
-    i32.segment_store (local.get 0) (i32.const 5)
+    (i32.store (local.get 0) (i32.const 5))
     local.get 0
-    i32.segment_load
+    i32.load
+    local.get 0
     call $__free
   )
+  (memory 1)
 )
