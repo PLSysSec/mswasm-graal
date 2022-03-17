2e96ebb91da9b9adfafd19e8f9aa379f13e15840
Alexandra Michael <aemichae@ucsd.edu>
2022-03-16 18:01:27 -0700

    Turned on compilation tracing

--- wasm/src/org.graalvm.wasm.test/src/org/graalvm/wasm/test/WasmFileSuite.java
+++ wasm/src/org.graalvm.wasm.test/src/org/graalvm/wasm/test/WasmFileSuite.java
@@ -105,6 +105,8 @@ public abstract class WasmFileSuite extends AbstractWasmSuite {
     private static Context getInterpretedNoInline(Context.Builder contextBuilder) {
         contextBuilder.option("engine.Compilation", "false");
         contextBuilder.option("engine.Inlining", "false");
+
+        contextBuilder.option("engine.TraceCompilationDetails", "true");
         return contextBuilder.build();
     }
 
@@ -113,6 +115,10 @@ public abstract class WasmFileSuite extends AbstractWasmSuite {
         contextBuilder.option("engine.BackgroundCompilation", "false");
         contextBuilder.option("engine.CompileImmediately", "true");
         contextBuilder.option("engine.Inlining", "false");
+
+        contextBuilder.option("engine.TraceCompilationDetails", "true");
+        contextBuilder.option("engine.TraceCompilationAST", "true");
+        contextBuilder.option("engine.PrintExpansionHistogram", "true");
         return contextBuilder.build();
     }
 
@@ -121,6 +127,10 @@ public abstract class WasmFileSuite extends AbstractWasmSuite {
         contextBuilder.option("engine.BackgroundCompilation", "false");
         contextBuilder.option("engine.CompileImmediately", "true");
         contextBuilder.option("engine.Inlining", "true");
+
+        contextBuilder.option("engine.TraceCompilationDetails", "true");
+        contextBuilder.option("engine.TraceCompilationAST", "true");
+        contextBuilder.option("engine.PrintExpansionHistogram", "true");
         return contextBuilder.build();
     }
 
@@ -130,6 +140,10 @@ public abstract class WasmFileSuite extends AbstractWasmSuite {
         contextBuilder.option("engine.CompileImmediately", "false");
         contextBuilder.option("engine.Inlining", "false");
         contextBuilder.option("engine.FirstTierCompilationThreshold", "100");
+
+        contextBuilder.option("engine.TraceCompilationDetails", "true");
+        contextBuilder.option("engine.TraceCompilationAST", "true");
+        contextBuilder.option("engine.PrintExpansionHistogram", "true");
         return contextBuilder.build();
     }
 
@@ -352,21 +366,21 @@ public abstract class WasmFileSuite extends AbstractWasmSuite {
             // Run in synchronous compiled mode, with inlining turned off.
             // We need to run the test at least twice like this, since the first run will lead to
             // de-opts due to empty profiles.
-            // int syncNoinlineIterations = Integer.parseInt(testCase.options().getProperty("sync-noinline-iterations", String.valueOf(DEFAULT_SYNC_NOINLINE_ITERATIONS)));
-            // context = getSyncCompiledNoInline(contextBuilder);
-            // runInContext(testCase, context, sources, syncNoinlineIterations, PHASE_SYNC_NO_INLINE_ICON, "sync,no-inl");
+            int syncNoinlineIterations = Integer.parseInt(testCase.options().getProperty("sync-noinline-iterations", String.valueOf(DEFAULT_SYNC_NOINLINE_ITERATIONS)));
+            context = getSyncCompiledNoInline(contextBuilder);
+            runInContext(testCase, context, sources, syncNoinlineIterations, PHASE_SYNC_NO_INLINE_ICON, "sync,no-inl");
 
             // Run in synchronous compiled mode, with inlining turned on.
             // We need to run the test at least twice like this, since the first run will lead to
             // de-opts due to empty profiles.
-            // int syncInlineIterations = Integer.parseInt(testCase.options().getProperty("sync-inline-iterations", String.valueOf(DEFAULT_SYNC_INLINE_ITERATIONS)));
-            // context = getSyncCompiledWithInline(contextBuilder);
-            // runInContext(testCase, context, sources, syncInlineIterations, PHASE_SYNC_INLINE_ICON, "sync,inl");
+            int syncInlineIterations = Integer.parseInt(testCase.options().getProperty("sync-inline-iterations", String.valueOf(DEFAULT_SYNC_INLINE_ITERATIONS)));
+            context = getSyncCompiledWithInline(contextBuilder);
+            runInContext(testCase, context, sources, syncInlineIterations, PHASE_SYNC_INLINE_ICON, "sync,inl");
 
             // Run with normal, asynchronous compilation.
-            // int asyncIterations = Integer.parseInt(testCase.options().getProperty("async-iterations", String.valueOf(DEFAULT_ASYNC_ITERATIONS)));
-            // context = getAsyncCompiled(contextBuilder);
-            // runInContext(testCase, context, sources, asyncIterations, PHASE_ASYNC_ICON, "async,multi");
+            int asyncIterations = Integer.parseInt(testCase.options().getProperty("async-iterations", String.valueOf(DEFAULT_ASYNC_ITERATIONS)));
+            context = getAsyncCompiled(contextBuilder);
+            runInContext(testCase, context, sources, asyncIterations, PHASE_ASYNC_ICON, "async,multi");
         } catch (InterruptedException | IOException e) {
             Assert.fail(String.format("Test %s failed: %s", testCase.name(), e.getMessage()));
         } finally {
